826f22e56765b82ebc682fadf3725b2bee9d8175
jdeleon247 <jdeleon247@gmail.com>
2025-05-01 17:18:48 -0400

    Added shell script

--- version_control_1/python/main.py
+++ version_control_1/python/main.py
@@ -4,4 +4,12 @@ from nicegui import ui
 
 ui.label('Hello NiceGUI!')
 
+with(ui.timeline(side='right')):
+        ui.timeline_entry(ui.textarea(label='Text', placeholder='start typing',
+            on_change=lambda e: result.set_text('you typed: ' + e.value)))
+        result = ui.label()
+
+
+
+
 ui.run()
